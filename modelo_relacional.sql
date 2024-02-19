5ab9fcd5c57fac907cc65ec311395b37b0f2a5a7
Edenilson Gomes <edenilsonngomes@21gmail.com>
2024-02-19 14:45:37 -0300

    finalização do projeto

--- modelo_relacional.sql
+++ modelo_relacional.sql
@@ -27,13 +27,11 @@ CREATE TABLE dim_espaco (
 );
 
 CREATE TABLE especies_ameacadas_fato (
-	id SERIAL PRIMARY KEY,
 	id_dim_especie INT REFERENCES dim_especie(id),
 	id_dim_ameaca INT REFERENCES dim_ameaca(id),
 	id_dim_espaco INT REFERENCES dim_espaco(id),
 	id_dim_tempo INT REFERENCES dim_tempo(id),
 	
-	quant_total_especies_ameacadas int,
-	quant_total_especies_criticamente_em_perigo int
+	quant_total_especies_ameacadas int
 	
 );
